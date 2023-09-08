b2b633c8f1ec31a0a478fd1fa4364d0f6fabb758
iamam305 <ajitesh314mishra@gmail.com>
2023-09-08 18:56:26 +0530

    suii

--- src/components/outerLayout/Navbar.tsx
+++ src/components/outerLayout/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       const res = await axios.get("/api/users/logout");
       localStorage.removeItem("user");
       router.push("/");
-      window.location.reload()
+      window.location.reload();
       toast.success(res.data.message);
     } catch (error: any) {
       toast.success(error.message);
@@ -40,49 +40,47 @@ const Navbar = () => {
           {/* Mobile menu button */}
           <div className="flex lg:hidden">
             <button
+              onClick={() => setIsOpen(!isOpen)}
               type="button"
               className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
               aria-label="toggle menu"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 8h16M4 16h16"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              {isOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
             </button>
           </div>
         </div>
         {/* Mobile Menu open: "block", Menu closed: "hidden" */}
-        <div
-          className={`${
-            isOpen
-              ? "translate-x-0 opacity-100 "
-              : "opacity-0 -translate-x-full"
-          }`}
-        >
+        <div className={`${isOpen ? "block" : "hidden"} md:block`}>
           <div className="flex flex-col md:flex-row md:mx-6">
             <Link
               className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
